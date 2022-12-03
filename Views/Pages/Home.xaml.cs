94dc19e80fb924478c2831154660806ffa10deac
Hồ Đình Mạnh <97241076+ManhHoDinh@users.noreply.github.com>
2022-12-03 23:47:56 +0700

    update navigation and bottom (#10)
    * Update MainWindow
    
    * "binh"
    
    * Update Navigation
    
    * BottomComponent
    
    * update volume button
    
    * color hover
    
    * update2
    
    * Navigation Button
    
    * Navigation Button (#7)
    
    * Navigation Button (#7)
    
    * Update bottom in mainwindown
    
    * Update MainWindow.xaml

--- Views/Pages/Home.xaml.cs
+++ Views/Pages/Home.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,20 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Spotify
+namespace Spotify.Views.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Home.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Home : UserControl
     {
-        public MainWindow()
+        public Home()
         {
-            InitializeComponent();   
-        }
-        private void CloseRightSideBarButton_Click(object sender, RoutedEventArgs e)
-        {
-            LoadSideBar.ColumnDefinitions.RemoveAt(1);
+            InitializeComponent();
         }
     }
 }
