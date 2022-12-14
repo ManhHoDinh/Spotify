5de62f539919ecdca85d4ad741334041fa76640c
Hồ Đình Mạnh <97241076+ManhHoDinh@users.noreply.github.com>
2022-12-14 08:27:30 +0700

    Choose music to bottom (#27)
    Co-authored-by: binh <binh93phudong&=@gmail.com>
    Co-authored-by: vocongbinh <105732042+vocongbinh@users.noreply.github.com>
    Co-authored-by: TuongPhamCT <tuongpt12503@gmail.com>

---
+++ Views/Components/NavigateButton.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spotify.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for NavigateButton.xaml
+    /// </summary>
+    public partial class NavigateButton : UserControl
+    {
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register
+            ("IconSource", typeof(ImageSource), typeof(NavigateButton), new PropertyMetadata(null));
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register
+            ("NavigateText", typeof(string), typeof(NavigateButton), new PropertyMetadata(null));
+        private ImageSource _iconSource;
+        private string _navigateText;
+        public ImageSource IconSource { 
+                get{
+                    return (ImageSource)GetValue(ImageProperty);
+                }
+                set
+                {
+                    _iconSource = value;
+                }
+            }
+        public string NavigateText {
+            get{
+                return (string)GetValue(TextProperty);
+               }
+            set
+            {
+                _navigateText = value;
+            }
+        }
+        
+        public NavigateButton()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
