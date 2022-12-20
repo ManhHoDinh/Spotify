4038575c9739c8ec49cc4905c2fa2289e5bec507
Hồ Đình Mạnh <97241076+ManhHoDinh@users.noreply.github.com>
2022-12-20 21:14:36 +0700

    Update navigation (#31) (#33)
    Co-authored-by: binh <binh93phudong&=@gmail.com>

---
+++ Views/Pages/SongView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spotify.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for SongView.xaml
+    /// </summary>
+    public partial class SongView : UserControl
+    {
+        public SongView()
+        {
+            InitializeComponent();
+            Random r = new Random();
+            Color RandomColor = Color.FromRgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 233));
+            string hex = RandomColor.R.ToString("X2") + RandomColor.G.ToString("X2") + RandomColor.B.ToString("X2");
+            color = "#" + hex;
+        }
+
+
+        public string color
+        {
+            get { return (string)GetValue(colorProperty); }
+            set { SetValue(colorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for color.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty colorProperty =
+            DependencyProperty.Register("color", typeof(string), typeof(SongView), new PropertyMetadata("red"));
+    }
+}
